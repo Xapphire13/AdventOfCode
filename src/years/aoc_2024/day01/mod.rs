6a46e318fe1ac82b940952c7198efbbe8c8f20f2
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-12-02 00:30:31 -0800

    2024 Day01 Pt2

--- src/years/aoc_2024/day01/mod.rs
+++ src/years/aoc_2024/day01/mod.rs
@@ -1,25 +1,33 @@
+use std::collections::HashMap;
+
 use crate::aoc_solution::Solution;
 
 pub struct Day1;
 
+fn parse_input(input: &str) -> [Vec<u32>; 2] {
+    let mut list1: Vec<u32> = Vec::new();
+    let mut list2: Vec<u32> = Vec::new();
+
+    input.lines().for_each(|line| {
+        if let [part1, part2, ..] = line.split("   ").collect::<Vec<_>>().as_slice() {
+            if let Ok(num1) = part1.parse() {
+                list1.push(num1);
+            }
+            if let Ok(num2) = part2.parse() {
+                list2.push(num2);
+            }
+        }
+    });
+
+    list1.sort();
+    list2.sort();
+
+    [list1, list2]
+}
+
 impl Solution for Day1 {
     fn part1(&self, input: &str) -> String {
-        let mut list1: Vec<u32> = Vec::new();
-        let mut list2: Vec<u32> = Vec::new();
-
-        input.lines().for_each(|line| {
-            if let [part1, part2, ..] = line.split("   ").collect::<Vec<_>>().as_slice() {
-                if let Ok(num1) = part1.parse() {
-                    list1.push(num1);
-                }
-                if let Ok(num2) = part2.parse() {
-                    list2.push(num2);
-                }
-            }
-        });
-
-        list1.sort();
-        list2.sort();
+        let [list1, list2] = parse_input(input);
 
         let mut result = 0u32;
 
@@ -30,11 +38,31 @@ impl Solution for Day1 {
             result += first.abs_diff(second);
         }
 
-        format!("{}", result)
+        result.to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        // Implement Part 2 solution
-        String::from("Not implemented")
+        let [list1, list2] = parse_input(input);
+
+        let mut frequencies: HashMap<u32, u32> = HashMap::new();
+
+        list2.iter().for_each(|num| {
+            if !frequencies.contains_key(num) {
+                frequencies.insert(*num, 0);
+            }
+
+            if let Some(val) = frequencies.get(num) {
+                frequencies.insert(*num, val + 1);
+            }
+        });
+
+        let mut result = 0;
+
+        list1.iter().for_each(|num| {
+            let frequency = frequencies.get(num).unwrap_or(&0);
+            result += num * frequency;
+        });
+
+        result.to_string()
     }
 }
