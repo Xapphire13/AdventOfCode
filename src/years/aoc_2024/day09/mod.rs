03ad735bcd03805d70550301b0385c5e4a6ce4cb
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-12-11 02:38:17 -0800

    [2024] Day 9 - pt2

--- src/years/aoc_2024/day09/mod.rs
+++ src/years/aoc_2024/day09/mod.rs
@@ -70,6 +70,86 @@ impl Problem {
         }
     }
 
+    fn defrag_2(&mut self) {
+        let mut left_ptr = 0;
+        let mut right_ptr = self.disk.len() - 1;
+
+        while left_ptr < right_ptr {
+            // Skip empty space
+            while let DiskBlock::Empty = self.disk[right_ptr] {
+                right_ptr -= 1;
+
+                if right_ptr == 0 {
+                    break;
+                }
+            }
+
+            if left_ptr >= right_ptr {
+                break;
+            }
+
+            // Find size of file
+            let mut file_size = 0;
+            let file_id = match self.disk[right_ptr] {
+                DiskBlock::File(file_id) => file_id,
+                DiskBlock::Empty => panic!("Expected file!"),
+            };
+            while let DiskBlock::File(id) = self.disk[right_ptr] {
+                if id != file_id {
+                    break;
+                }
+
+                file_size += 1;
+
+                if right_ptr == 0 {
+                    break;
+                }
+
+                right_ptr -= 1;
+            }
+            right_ptr += 1;
+
+            if left_ptr >= right_ptr {
+                break;
+            }
+
+            // Find space
+            let mut space = 0;
+            while space == 0 && left_ptr < right_ptr {
+                while let DiskBlock::File(_) = self.disk[left_ptr] {
+                    left_ptr += 1;
+                    if left_ptr == self.disk.len() {
+                        break;
+                    }
+                }
+
+                while let DiskBlock::Empty = self.disk[left_ptr + space] {
+                    space += 1;
+
+                    if left_ptr + space == self.disk.len() {
+                        break;
+                    }
+                }
+
+                if space < file_size {
+                    left_ptr += space;
+                    space = 0;
+                }
+            }
+
+            if left_ptr < right_ptr {
+                // Move file
+                for i in 0..file_size {
+                    self.disk[left_ptr + i] = self.disk[right_ptr + i].clone();
+                    self.disk[right_ptr + i] = DiskBlock::Empty;
+                }
+            }
+
+            left_ptr = 0;
+            right_ptr -= 1;
+        }
+    }
+
     fn checksum(&self) -> String {
         let mut result: u64 = 0;
 
@@ -95,7 +175,10 @@ impl Solution for Day9 {
     }
 
     fn part2(&self, input: &str) -> String {
-        String::from("Not implemented")
+        let mut problem = Problem::parse_input(input.trim());
+        problem.defrag_2();
+
+        problem.checksum()
     }
 }
 
@@ -104,9 +187,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day9() {
+    fn day9_pt1() {
         let result = Day9.part1("2333133121414131402");
 
         assert_eq!(result, "1928");
+    }
+
+    #[test]
+    fn day9_pt2() {
+        let result = Day9.part2("2333133121414131402");
+
+        assert_eq!(result, "2858");
     }
 }
