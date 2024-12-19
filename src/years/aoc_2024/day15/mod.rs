32e0f721091d3d173a5fea7ceb3c4631971f0331
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-12-19 00:57:30 -0800

    [2024] Day 15 - pt2

--- src/years/aoc_2024/day15/mod.rs
+++ src/years/aoc_2024/day15/mod.rs
@@ -14,22 +14,36 @@ impl Coordinate {
     fn gps_coordinate(&self) -> u32 {
         100 * self.y + self.x
     }
-}
 
-enum Tile {
-    Wall,
-    Box,
-    Robot,
-}
-
-impl Tile {
-    fn to_char(&self) -> char {
-        match self {
-            Tile::Wall => '#',
-            Tile::Box => 'O',
-            Tile::Robot => '@',
+    fn translate(&self, direction: Direction) -> Coordinate {
+        match direction {
+            Direction::Up => Coordinate {
+                x: self.x,
+                y: self.y.saturating_sub(1),
+            },
+            Direction::Down => Coordinate {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::Left => Coordinate {
+                x: self.x.saturating_sub(1),
+                y: self.y,
+            },
+            Direction::Right => Coordinate {
+                x: self.x + 1,
+                y: self.y,
+            },
         }
     }
+}
+
+#[derive(Debug)]
+enum Tile {
+    Wall,
+    UnitBox,
+    BoxLeft,
+    BoxRight,
+    Robot,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -44,32 +58,33 @@ struct Map {
     tiles: HashMap<Coordinate, Tile>,
     instructions: Vec<Direction>,
     robot_position: Coordinate,
-    width: u32,
-    height: u32,
 }
 
 impl Map {
-    fn parse_input(input: &str) -> Map {
+    fn parse_input(input: &str, tile_width: u8) -> Map {
         let mut lines = input.trim().lines();
         let mut tiles = HashMap::new();
         let mut robot_position = None;
-        let mut width = 0;
-        let mut height = 0;
 
         for (y, line) in lines
             .borrow_mut()
             .enumerate()
             .take_while(|(_, line)| !line.is_empty())
-        {
-            if y + 1 > height {
-                height = y + 1;
-            }
-
-            for (x, char) in line.chars().enumerate() {
-                if x + 1 > width {
-                    width = x + 1;
+            .map(|(y, line)| {
+                if tile_width == 1 {
+                    (y, line.to_string())
+                } else {
+                    (
+                        y,
+                        line.replace("#", "##")
+                            .replace("O", "[]")
+                            .replace(".", "..")
+                            .replace("@", "@."),
+                    )
                 }
-
+            })
+        {
+            for (x, char) in line.chars().enumerate() {
                 let position = Coordinate {
                     x: x as u32,
                     y: y as u32,
@@ -80,7 +95,13 @@ impl Map {
                         tiles.insert(position, Tile::Wall);
                     }
                     'O' => {
-                        tiles.insert(position, Tile::Box);
+                        tiles.insert(position, Tile::UnitBox);
+                    }
+                    '[' => {
+                        tiles.insert(position, Tile::BoxLeft);
+                    }
+                    ']' => {
+                        tiles.insert(position, Tile::BoxRight);
                     }
                     '@' => {
                         robot_position = Some(position);
@@ -109,88 +130,232 @@ impl Map {
             tiles,
             instructions,
             robot_position: robot_position.unwrap(),
-            width: width as u32,
-            height: height as u32,
         }
     }
 
-    fn move_tile(&mut self, current_position: Coordinate, direction: Direction) -> bool {
-        if !self.tiles.contains_key(&current_position) {
-            return false;
-        }
+    fn can_move(&mut self, current_position: Coordinate, direction: Direction) -> bool {
+        match self.tiles.get(&current_position) {
+            Some(Tile::BoxLeft) => match direction {
+                Direction::Up | Direction::Down => {
+                    let box_right_position = current_position.translate(Direction::Right);
 
-        let next_position = match direction {
-            Direction::Up => Coordinate {
-                x: current_position.x,
-                y: current_position.y.saturating_sub(1),
+                    return self.can_move(current_position.translate(direction), direction)
+                        && self.can_move(box_right_position.translate(direction), direction);
+                }
+                Direction::Left => {
+                    return self.can_move(current_position.translate(Direction::Left), direction);
+                }
+                Direction::Right => {
+                    return self.can_move(
+                        current_position
+                            .translate(Direction::Right)
+                            .translate(Direction::Right),
+                        direction,
+                    )
+                }
             },
-            Direction::Down => Coordinate {
-                x: current_position.x,
-                y: current_position.y + 1,
-            },
-            Direction::Left => Coordinate {
-                x: current_position.x.saturating_sub(1),
-                y: current_position.y,
-            },
-            Direction::Right => Coordinate {
-                x: current_position.x + 1,
-                y: current_position.y,
-            },
-        };
+            Some(Tile::BoxRight) => match direction {
+                Direction::Up | Direction::Down => {
+                    let box_left_position = current_position.translate(Direction::Left);
 
-        if let Some(tile_at_position) = self.tiles.get(&next_position) {
-            if let Tile::Wall = tile_at_position {
-                // Can't move through walls
+                    return self.can_move(box_left_position.translate(direction), direction)
+                        && self.can_move(current_position.translate(direction), direction);
+                }
+                Direction::Left => {
+                    return self.can_move(
+                        current_position
+                            .translate(Direction::Left)
+                            .translate(Direction::Left),
+                        direction,
+                    );
+                }
+                Direction::Right => {
+                    return self.can_move(current_position.translate(Direction::Right), direction)
+                }
+            },
+            Some(Tile::UnitBox) | Some(Tile::Robot) => {
+                return self.can_move(current_position.translate(direction), direction)
+            }
+            Some(Tile::Wall) => {
+                // Can't move walls
                 return false;
             }
-
-            if !self.move_tile(next_position, direction) {
-                // We couldn't move the stuff in the way!
-                return false;
-            }
+            None => {} // Emptiness isn't in the way
         }
-
-        let removed_tile = self.tiles.remove(&current_position).unwrap();
-        if let Tile::Robot = removed_tile {
-            self.robot_position = next_position;
-        }
-        self.tiles.insert(next_position, removed_tile);
 
         true
     }
 
-    fn print(&self) {
-        for y in 0..self.height {
-            for x in 0..self.width {
-                match self.tiles.get(&Coordinate { x, y }) {
-                    Some(tile) => print!("{}", tile.to_char()),
-                    None => print!("."),
+    fn move_tile(&mut self, current_position: Coordinate, direction: Direction) {
+        let can_move = self.can_move(current_position, direction);
+
+        if !can_move {
+            return;
+        }
+
+        let next_position = current_position.translate(direction);
+
+        // Move tiles in the way first
+        match self.tiles.get(&current_position) {
+            Some(Tile::BoxLeft) => match direction {
+                Direction::Up | Direction::Down => {
+                    self.move_tile(next_position, direction);
+                    self.move_tile(next_position.translate(Direction::Right), direction);
                 }
+                Direction::Left => {
+                    self.move_tile(next_position, direction);
+                }
+                Direction::Right => {
+                    self.move_tile(next_position.translate(Direction::Right), direction);
+                }
+            },
+            Some(Tile::BoxRight) => match direction {
+                Direction::Up | Direction::Down => {
+                    self.move_tile(next_position, direction);
+                    self.move_tile(next_position.translate(Direction::Left), direction);
+                }
+                Direction::Left => {
+                    self.move_tile(next_position.translate(Direction::Left), direction);
+                }
+                Direction::Right => {
+                    self.move_tile(next_position, direction);
+                }
+            },
+            Some(Tile::UnitBox) => {
+                self.move_tile(next_position, direction);
             }
-            println!();
+            Some(Tile::Robot) => {
+                self.move_tile(next_position, direction);
+            }
+            Some(Tile::Wall) => {} // Don't attempt to move walls
+            None => {}             // Don't attempt to move emptiness
+        }
+
+        match self.tiles.get(&current_position) {
+            Some(Tile::BoxLeft) => {
+                let removed_left_tile = self.tiles.remove(&current_position).unwrap();
+                let removed_right_tile = self
+                    .tiles
+                    .remove(&current_position.translate(Direction::Right))
+                    .unwrap();
+                self.tiles.insert(next_position, removed_left_tile);
+                self.tiles.insert(
+                    next_position.translate(Direction::Right),
+                    removed_right_tile,
+                );
+            }
+            Some(Tile::BoxRight) => {
+                let removed_left_tile = self
+                    .tiles
+                    .remove(&current_position.translate(Direction::Left))
+                    .unwrap();
+                let removed_right_tile = self.tiles.remove(&current_position).unwrap();
+                self.tiles
+                    .insert(next_position.translate(Direction::Left), removed_left_tile);
+                self.tiles.insert(next_position, removed_right_tile);
+            }
+            Some(Tile::UnitBox) => {
+                let removed_tile = self.tiles.remove(&current_position).unwrap();
+                self.tiles.insert(next_position, removed_tile);
+            }
+            Some(Tile::Robot) => {
+                let removed_tile = self.tiles.remove(&current_position).unwrap();
+                self.tiles.insert(next_position, removed_tile);
+                self.robot_position = next_position;
+            }
+            Some(Tile::Wall) => {} // Don't attempt to move walls
+            None => {}             // Don't attempt to move emptiness
         }
     }
 }
 
 impl Solution for Day15 {
     fn part1(&self, input: &str) -> String {
-        let mut map = Map::parse_input(input);
+        let mut map = Map::parse_input(input, 1);
 
         for direction in map.instructions.clone() {
             map.move_tile(map.robot_position, direction);
         }
 
-        map.print();
-
         map.tiles
             .iter()
-            .filter(|(_, tile)| matches!(tile, Tile::Box))
+            .filter(|(_, tile)| matches!(tile, Tile::UnitBox))
             .map(|(coordinate, _)| coordinate.gps_coordinate())
             .sum::<u32>()
             .to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        String::from("Not implemented")
+        let mut map = Map::parse_input(input, 2);
+
+        for direction in map.instructions.clone() {
+            map.move_tile(map.robot_position, direction);
+        }
+
+        map.tiles
+            .iter()
+            .filter(|(_, tile)| matches!(tile, Tile::BoxLeft))
+            .map(|(coordinate, _)| coordinate.gps_coordinate())
+            .sum::<u32>()
+            .to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{thread::sleep, time::Duration};
+
+    use dedent::dedent;
+
+    use super::*;
+
+    impl Tile {
+        fn to_str(&self) -> char {
+            match self {
+                Tile::Wall => '#',
+                Tile::UnitBox => 'O',
+                Tile::Robot => '@',
+                Tile::BoxLeft => '[',
+                Tile::BoxRight => ']',
+            }
+        }
+    }
+
+    impl Map {
+        fn print(&self, width: u32, height: u32) {
+            for y in 0..height {
+                for x in 0..width {
+                    match self.tiles.get(&Coordinate { x, y }) {
+                        Some(tile) => print!("{}", tile.to_str()),
+                        None => print!("."),
+                    }
+                }
+                println!();
+            }
+        }
+    }
+
+    #[test]
+    fn test_day15() {
+        let input = dedent!(
+            "
+            #####
+            #...#
+            #O..#
+            #.O.#
+            #.@.#
+            #####
+
+            >>^<v<^^
+            "
+        );
+
+        let mut map = Map::parse_input(input, 2);
+
+        for direction in map.instructions.clone() {
+            map.move_tile(map.robot_position, direction);
+            map.print(10, 6);
+            sleep(Duration::from_millis(500));
+        }
     }
 }
