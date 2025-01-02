199355c569535d53dc47044d29df85cc46a1cce0
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2025-01-01 23:47:18 -0800

    [2024] Day 20 - pt1

---
+++ src/years/aoc_2024/day20/mod.rs
@@ -0,0 +1,206 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::aoc_solution::Solution;
+
+pub struct Day20;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Empty,
+    Wall,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct XYCoordindate(usize, usize);
+
+impl XYCoordindate {
+    fn up(&self) -> XYCoordindate {
+        let XYCoordindate(x, y) = *self;
+
+        XYCoordindate(x, y.saturating_sub(1))
+    }
+    fn down(&self) -> XYCoordindate {
+        let XYCoordindate(x, y) = *self;
+
+        XYCoordindate(x, y + 1)
+    }
+    fn left(&self) -> XYCoordindate {
+        let XYCoordindate(x, y) = *self;
+
+        XYCoordindate(x.saturating_sub(1), y)
+    }
+    fn right(&self) -> XYCoordindate {
+        let XYCoordindate(x, y) = *self;
+
+        XYCoordindate(x + 1, y)
+    }
+}
+
+struct Problem {
+    map: Vec<Vec<Tile>>,
+    start_position: XYCoordindate,
+    end_position: XYCoordindate,
+}
+
+impl Problem {
+    fn parse_input(input: &str) -> Problem {
+        let mut start_position = XYCoordindate(0, 0);
+        let mut end_position = XYCoordindate(0, 0);
+        let map = input
+            .lines()
+            .enumerate()
+            .map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(x, char)| match char {
+                        '#' => Tile::Wall,
+                        char => {
+                            if char == 'S' {
+                                start_position = XYCoordindate(x, y);
+                            } else if char == 'E' {
+                                end_position = XYCoordindate(x, y);
+                            }
+
+                            Tile::Empty
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        Problem {
+            map,
+            start_position,
+            end_position,
+        }
+    }
+
+    fn get(&self, XYCoordindate(x, y): XYCoordindate) -> Option<Tile> {
+        self.map.get(y).and_then(|row| row.get(x).cloned())
+    }
+
+    fn race(&self) -> Vec<XYCoordindate> {
+        let mut location_history = HashSet::new();
+        location_history.insert(self.start_position);
+
+        let mut result = vec![self.start_position];
+
+        while let Some(current_position) = result.last() {
+            if *current_position == self.end_position {
+                break;
+            }
+
+            let next_position = [
+                current_position.left(),
+                current_position.right(),
+                current_position.up(),
+                current_position.down(),
+            ]
+            .into_iter()
+            .find(|next_position| {
+                !location_history.contains(next_position)
+                    && self
+                        .get(*next_position)
+                        .is_some_and(|tile| tile != Tile::Wall)
+            })
+            .unwrap();
+
+            location_history.insert(next_position);
+            result.push(next_position);
+        }
+
+        result
+    }
+}
+
+impl Solution for Day20 {
+    fn part1(&self, input: &str) -> String {
+        let problem = Problem::parse_input(input);
+        let path_without_cheating = problem.race();
+        let mut result = vec![];
+        let mut location_to_time = HashMap::new();
+
+        path_without_cheating
+            .iter()
+            .enumerate()
+            .for_each(|(i, position)| {
+                location_to_time.insert(position, i);
+            });
+
+        let mut cheat_starting_positions = HashSet::new();
+        for position in path_without_cheating.iter() {
+            let cheat_start_time = *location_to_time.get(position).unwrap();
+
+            for (cheat_start_pos, cheat_end_pos) in [
+                (position.left(), position.left().left()),
+                (position.right(), position.right().right()),
+                (position.up(), position.up().up()),
+                (position.down(), position.down().down()),
+            ] {
+                if cheat_starting_positions.contains(&cheat_start_pos) {
+                    continue;
+                }
+
+                cheat_starting_positions.insert(cheat_start_pos);
+
+                if let Some(cheat_end) = problem.get(cheat_start_pos).and_then(|cheat_start| {
+                    if cheat_start == Tile::Wall {
+                        problem.get(cheat_end_pos)
+                    } else {
+                        None
+                    }
+                }) {
+                    if cheat_end == Tile::Empty {
+                        if let Some(cheat_end_time) = location_to_time.get(&cheat_end_pos) {
+                            // Plus 1 to account for walking through the wall
+                            let time_savings = cheat_end_time.saturating_sub(cheat_start_time + 1);
+
+                            if time_savings >= 100 {
+                                result.push(time_savings);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        result.len().to_string()
+    }
+
+    fn part2(&self, input: &str) -> String {
+        String::from("Not implemented")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use dedent::dedent;
+
+    use super::*;
+
+    #[test]
+    fn test_day20() {
+        let input = dedent!(
+            "
+            ###############
+            #...#...#.....#
+            #.#.#.#.#.###.#
+            #S#...#.#.#...#
+            #######.#.#.###
+            #######.#.#...#
+            #######.#.###.#
+            ###..E#...#...#
+            ###.#######.###
+            #...###...#...#
+            #.#####.#.###.#
+            #.#...#.#.#...#
+            #.#.#.#.#.#.###
+            #...#...#...###
+            ###############
+            "
+        );
+        let result = Day20.part1(input);
+
+        assert_eq!(result, "64");
+    }
+}
