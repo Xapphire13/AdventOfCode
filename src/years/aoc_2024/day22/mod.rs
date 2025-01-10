537dd8d8dbc7785f6865d631047bf919dde23230
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2025-01-09 21:54:28 -0800

    [2024] Day 22 - pt2

--- src/years/aoc_2024/day22/mod.rs
+++ src/years/aoc_2024/day22/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 use crate::aoc_solution::Solution;
@@ -10,6 +12,7 @@ struct Problem {
 
 trait Secret {
     fn next(&self) -> Self;
+    fn price(&self) -> Self;
 }
 
 impl Secret for u64 {
@@ -26,6 +29,10 @@ impl Secret for u64 {
         result %= 16777216;
 
         result
+    }
+
+    fn price(&self) -> Self {
+        self % 10
     }
 }
 
@@ -64,6 +71,57 @@ impl Solution for Day22 {
     }
 
     fn part2(&self, input: &str) -> String {
-        String::from("Not implemented")
+        let problem = Problem::parse_input(input);
+        let mut total_bananas = HashMap::new();
+
+        for secret in problem.secrets {
+            let mut current = secret;
+            let mut prices = vec![];
+            let mut change_sequences = HashMap::new();
+            for _ in 0..2000 {
+                let next = current.next();
+                let next_price = next.price();
+                prices.push((next_price, next_price as i64 - current.price() as i64));
+                current = next;
+            }
+
+            for window in prices.windows(4) {
+                let price = window[3].0;
+                let changes = (window[0].1, window[1].1, window[2].1, window[3].1);
+
+                change_sequences.entry(changes).or_insert(price);
+            }
+
+            for sequence in change_sequences {
+                total_bananas
+                    .entry(sequence.0)
+                    .and_modify(|bananas| *bananas += sequence.1)
+                    .or_insert(sequence.1);
+            }
+        }
+
+        total_bananas.values().max().unwrap().to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use dedent::dedent;
+
+    use super::*;
+
+    #[test]
+    fn test_day22_part2() {
+        let input = dedent!(
+            "
+            5053
+            10083
+            11263
+            "
+        );
+
+        let result = Day22.part2(input);
+
+        assert_eq!(result, "27");
     }
 }
