8022850f2284c9d44a85b4c9ebc255613aba9134
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2025-12-11 00:11:30 -0800

    [2025] day03, pt2

--- aoc-2025/src/day03/mod.rs
+++ aoc-2025/src/day03/mod.rs
@@ -29,8 +29,46 @@ impl Solution for Day3 {
     }
 
     fn part2(&self, input: &str) -> String {
-        String::from("todo")
+        let mut result = 0u64;
+        let banks = parse_input(input);
+
+        for bank in banks {
+            let mut digits_remaining = 12;
+            let mut batteries = vec![];
+            let mut start_index = 0;
+
+            while digits_remaining > 0 {
+                let (index, digit) = pick_digit(&bank[start_index..], digits_remaining - 1);
+                batteries.push(digit);
+                start_index += index + 1;
+
+                digits_remaining -= 1;
+            }
+
+            let joltage: u64 = batteries
+                .iter()
+                .map(|battery| battery.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+                .parse()
+                .unwrap();
+
+            result += joltage;
+        }
+
+        result.to_string()
     }
+}
+
+fn pick_digit(bank: &[u8], reserved_space: usize) -> (usize, u8) {
+    let (index, value) = bank[..bank.len() - reserved_space]
+        .iter()
+        .enumerate()
+        .rev()
+        .max_by_key(|pair| pair.1)
+        .unwrap();
+
+    (index, *value)
 }
 
 fn parse_input(input: &str) -> Vec<Vec<u8>> {
