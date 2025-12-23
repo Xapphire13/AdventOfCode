ec5a93b4114cba685dd7ef2131883237d7c2f085
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2025-12-23 15:35:06 -0800

    [2025] day 6, pt2

--- aoc-2025/src/day06/mod.rs
+++ aoc-2025/src/day06/mod.rs
@@ -27,7 +27,9 @@ impl Solution for Day6 {
     }
 
     fn part2(&self, input: &str) -> String {
-        String::from("todo")
+        let problem_set = ProblemSet::new_alt(input);
+
+        problem_set.grand_total().to_string()
     }
 }
 
@@ -81,7 +83,66 @@ impl ProblemSet {
         Self { problems }
     }
 
+    fn new_alt(input: &str) -> Self {
+        let mut problems = vec![];
+        let mut lines = input.trim().lines().collect::<Vec<_>>();
+        let operator_line = *lines.last().unwrap();
+        let mut problem_widths = vec![];
+
+        // Figure out the width of each problem
+        let mut problem_width = 0usize;
+        for c in operator_line.chars() {
+            if !c.is_whitespace() {
+                if problem_width > 0 {
+                    problem_widths.push(problem_width - 1);
+                }
+
+                problem_width = 1;
+
+                problems.push(Problem {
+                    operator: Operator::parse(&c.to_string()),
+                    values: vec![],
+                });
+            } else {
+                problem_width += 1;
+            }
+        }
+
+        let longest_line = lines.iter().map(|line| line.len()).max().unwrap();
+        let last_problem_width = longest_line - operator_line.len() + 1;
+        problem_widths.push(last_problem_width);
+
+        lines.pop(); // Remove the operator line
+
+        // For each problem, get the constituent numbers
+        let mut col = 0;
+        for (index, &width) in problem_widths.iter().enumerate() {
+            let problem = &mut problems[index];
+
+            for _ in 0..width {
+                let number = ProblemSet::get_vertical_number(&lines, col);
+                problem.values.push(number);
+                col += 1;
+            }
+
+            col += 1;
+        }
+
+        Self { problems }
+    }
+
     fn grand_total(&self) -> usize {
         self.problems.iter().map(|problem| problem.solve()).sum()
+    }
+
+    fn get_vertical_number(data: &Vec<&str>, col: usize) -> usize {
+        let mut buf = vec![];
+
+        for &row in data {
+            let c = row.chars().nth(col).unwrap_or('0').to_string();
+            buf.push(c);
+        }
+
+        buf.join("").trim().parse().unwrap_or(0)
     }
 }
